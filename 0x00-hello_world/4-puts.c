21ec1fc2a10aa255245c40d5b881eb34baa515ff
moallaa <moalaa@mo.com>
2023-07-13 19:19:26 -0700

    moalaa

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,15 @@
 #include <stdio.h>
+
 /**
- *  main - moalaa
- * return: always 0 (moalaa)
- */
+ * main - Entry point
+ *
+ * Descriotion: A C program that prints with put functin
+ *
+ * Return: Always 0 (success)
+*/
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-        return (0);
-}       
+	return (0);
+}
