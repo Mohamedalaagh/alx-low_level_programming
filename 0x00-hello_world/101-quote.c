09dbd92bdbb1023887b2a131422b89c85c1bf6cd
moallaa <moalaa@mo.com>
2023-07-17 04:48:59 -0700

    moalaa

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,7 +1,18 @@
 #include <unistd.h>
 
-int main() {
-    char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-    write(STDERR_FILENO, message, sizeof(message) - 1);
-    return 1;
-}	
+/**
+ * main - Entry point
+ *
+ * Description: A C program that prints using the write function
+ *
+ * Return: Always 1 (error)
+ */
+
+int main(void)
+{
+	char message[] = "and that piece of art is useful\" -	Dora Korparku
+	, 2015-10-19\n";
+	write(STDERR_FILENO, message, sizeof(message) - 1);
+
+	return (1);
+}
