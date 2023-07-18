4b4ae4a009f9eca5a82fb3f08f192c90ae2c129c
moallaa <moalaa@mo.com>
2023-07-18 16:31:44 -0700

    moalaa

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,14 +1,21 @@
-#include <stdio.h>
+#include "main.h"
+
 /**
  * main - entry point
  *
- * Descriptin: puchar
+ * Description: print _puchar
  *
  * Return: 0 (success)
 */
 
 int main(void)
 {
-	printf("_putchar\n");
+	char str[] = "_puchar";
+	int ch;
+
+	for (ch=0; ch < 8;ch++)
+		_putchar(str[ch]);
+	_putchar("\n");
+
 	return (0);
 }
---
+++ 0x02-functions_nested_loops/_putchar.c
@@ -0,0 +1,14 @@
+#include <unistd.h>
+#include "main.h"
+/**
+ * _putchar - write the charcter c to the stdout
+ * @c: the character to print
+ *
+ * Return: on success 1
+ *on error, -1 is returned
+*/
+
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
