bccd3c3d5d2e67273fce392f5df130e18c6e5014
Your Name <you@example.com>
2023-12-21 11:08:17 -0800

    moalaaa

---
+++ 0x1A-hash_tables/2-key_index.c
@@ -0,0 +1,14 @@
+#include "hash_tables.h"
+
+/**
+ * key_index - returns the index of a key.
+ * @key: pointer to key to get index.
+ * @size: size of the array of the hash table.
+ *
+ * Return: index at which the key/value pairs,
+ * array of the hash table.
+ */
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	return (hash_djb2(key) % size);
+}
