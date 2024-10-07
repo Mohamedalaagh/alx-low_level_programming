36cb8bb36a307b2c7e22049f6bcaafd9594e0b05
Your Name <you@example.com>
2024-10-07 23:22:22 +0300

    done

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,10 +8,12 @@
  * Return: Always 1 (error)
  */
 
-int main() {
-	char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	
+int main(void)
+{
+	char message[] = "and that piece of art is useful
+		\" - Dora Korpar, 2015-10-19\n";
+
 	write(STDERR_FILENO, message, sizeof(message) - 1);
-	
-	return 1;
+
+	return (1);
 }
